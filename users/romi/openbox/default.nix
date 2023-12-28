06c14627db3cb1c09024822ad7209072c3622fc7
Romi Hervier <r@sansfontieres.com>
2023-12-28 20:21:09 +0100

    Tidy openbox config

--- users/romi/openbox/default.nix
+++ users/romi/openbox/default.nix
@@ -1,12 +1,12 @@
-{config, ...}: let
-  theme_path = "${config.home.homeDirectory}/.themes/theme/openbox-3";
+let
+  theme_path = "themes/theme/openbox-3";
 in {
   xdg.configFile."openbox/rc.xml".source = ./openbox.xml;
-  home.file."${theme_path}/bullet.xbm".source = ./bullet.xbm;
-  home.file."${theme_path}/close.xbm".source = ./close.xbm;
-  home.file."${theme_path}/iconify.xbm".source = ./iconify.xbm;
-  home.file."${theme_path}/max_toggled.xbm".source = ./max_toggled.xbm;
-  home.file."${theme_path}/max.xbm".source = ./max.xbm;
-  home.file."${theme_path}/shade.xbm".source = ./shade.xbm;
-  home.file."${theme_path}/themerc".source = ./themerc;
+  xdg.dataFile."${theme_path}/bullet.xbm".source = ./bullet.xbm;
+  xdg.dataFile."${theme_path}/close.xbm".source = ./close.xbm;
+  xdg.dataFile."${theme_path}/iconify.xbm".source = ./iconify.xbm;
+  xdg.dataFile."${theme_path}/max_toggled.xbm".source = ./max_toggled.xbm;
+  xdg.dataFile."${theme_path}/max.xbm".source = ./max.xbm;
+  xdg.dataFile."${theme_path}/shade.xbm".source = ./shade.xbm;
+  xdg.dataFile."${theme_path}/themerc".source = ./themerc;
 }
